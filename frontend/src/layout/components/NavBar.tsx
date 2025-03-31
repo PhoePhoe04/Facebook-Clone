6a9d7e059deff044638bdbed7d2fff4595e73898
lac2304 <loihuu114@gmail.com>
2025-03-31 16:18:42 +0700

    thg cho nao xoa su kien cua bo may

--- frontend/src/layout/components/NavBar.tsx
+++ frontend/src/layout/components/NavBar.tsx
@@ -19,7 +19,7 @@ const NavBar = () => {
 
   const [actived, setActived] = useState<string | null>(null);
 
-  // Cập nhật thái actived dựa trên đường dẫn hiện tại
+  // Cập nhật trạng thái actived dựa trên đường dẫn hiện tại
   useEffect(() => {
     const path = location.pathname;
     if (path === "/") setActived("home");
@@ -28,16 +28,17 @@ const NavBar = () => {
     else if (path === "/profile") setActived("profile");
     else setActived(null);
   }, [location.pathname]);
-  const handelIconClick = (iconName: string, path: string) => {
+
+  const handleIconClick = (iconName: string, path: string) => {
     setActived(iconName);
     navigate(path);
   };
 
   return (
     <div className="fixed top-0 left-0 right-0 bg-white shadow-md z-50 h-14">
-      <div className="flex w-full  mx-auto px-4">
-        {/* left */}
-        <div className="w-1/4 flex items-center justify-start space-x-4 ">
+      <div className="flex w-full mx-auto px-4">
+        {/* Left */}
+        <div className="w-1/4 flex items-center justify-start space-x-4">
           <img src={fblogo} alt="logo" className="size-10" />
           <input
             type="text"
@@ -45,16 +46,17 @@ const NavBar = () => {
             className="text-black bg-white rounded-2xl h-10 w-3/4 p-4"
           />
         </div>
+
         {/* Center */}
         <div className="w-1/2 flex items-center justify-center px-14">
-          <div className=" w-full h-full items-center justify-center hidden sm:flex">
+          <div className="w-full h-full items-center justify-center hidden sm:flex">
             <div
               className={`p-2 sm:p-3.5 cursor-pointer flex-1 flex justify-center hover:bg-gray-300 transition-colors ${
                 actived === "home"
-                  ? "border-b-4 border-blue-600 "
+                  ? "border-b-4 border-blue-600"
                   : "border-b-4 border-transparent"
               }`}
-              onClick={() => handelIconClick("home", "/")}
+              onClick={() => handleIconClick("home", "/")}
             >
               <img src={homefb} alt="Home" className="size-5 sm:size-6" />
             </div>
@@ -64,7 +66,7 @@ const NavBar = () => {
                   ? "border-b-4 border-blue-600"
                   : "border-b-4 border-transparent"
               }`}
-              onClick={() => handelIconClick("friends", "/friends")}
+              onClick={() => handleIconClick("friends", "/friends")}
             >
               <img src={friends} alt="Friends" className="size-5 sm:size-6" />
             </div>
@@ -74,12 +76,13 @@ const NavBar = () => {
                   ? "border-b-4 border-blue-600"
                   : "border-b-4 border-transparent"
               }`}
-              onClick={() => handelIconClick("videos", "/videos")}
+              onClick={() => handleIconClick("videos", "/videos")}
             >
               <img src={videos} alt="Videos" className="size-5 sm:size-6" />
             </div>
           </div>
         </div>
+
         {/* Right */}
         <div className="w-1/4 flex items-center justify-end">
           <div className="flex items-center space-x-2 sm:space-x-4">
@@ -92,10 +95,11 @@ const NavBar = () => {
             <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition">
               <BellIcon className="h-6 w-6 text-gray-800" />
             </button>
-            <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition">
+            <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition" onClick={() => handleIconClick("profile", "/profile")}>
               <UserCircleIcon className="h-6 w-6 text-gray-800" />
             </button>
           </div>
+        </div>
       </div>
     </div>
   );
